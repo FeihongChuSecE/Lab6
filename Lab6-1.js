31fdd8cf9b6c35b54d1b29fcdc47e71bd6f0e206
FeihongChuSecE <faye.chu.fh@gmail.com>
2024-10-08 10:30:30 -0400

    Add files via upload

---
+++ Lab6-1.js
@@ -0,0 +1,28 @@
+//task1
+let myNumber = 10;
+let myString = 'Hello, JavaScript!';
+console.log(myNumber);
+console.log(myString);
+
+//task2
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(3 * i);
+}
+
+//task3
+function addNumbers(num1, num2) {
+    return num1 + num2;
+};
+let result = addNumbers(10, 2);
+console.log(result);
+
+//task4
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+let result2 = multiplyNumbers(20, 3);
+console.log(result2);
